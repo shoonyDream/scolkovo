b3a6740e383f5e43a0656c2d78ef5186f067aac5
Denis <vbdenis@yandex.ru>
2020-01-23 14:51:09 +0300

    colors? icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,31 +1,45 @@
 import velocity from 'velocity-animate/velocity.min.js';
-$('.js-footer-header')
+$('.js-add-trigger')
 	.on('click', function(e) {
-		if ($(window).width() <= 1150) {
 
-			let $this = $(this),
-				$body = $this.next('.js-footer-body');
+		let $this = $(this),
+			$body = $this.next('.js-add-content');
 
-			if ($body.length) {
-				if ($body.is(':visible')) {
-					$body
-						// .velocity('stop')
-						.velocity("slideUp", {
-							duration: 300,
-							complete: function() {
-								$this.removeClass('footer__title--open');
-							}
-						})
-				} else {
-					$body
-						// .velocity('stop')
-						.velocity("slideDown", {
-							duration: 300,
-							complete: function() {
-								$this.addClass('footer__title--open');
-							}
-						})
-				}
+		if ($body.length) {
+			if ($body.is(':visible')) {
+				$body
+					// .velocity('stop')
+					.velocity("slideUp", {
+						duration: 300,
+						complete: function() {
+							$this.removeClass('js-add-content--open');
+						}
+					})
+			} else {
+				$body
+					// .velocity('stop')
+					.velocity("slideDown", {
+						duration: 300,
+						complete: function() {
+							$this.addClass('js-add-content--open');
+						}
+					})
 			}
 		}
+	})
+
+$('.additional-info__close')
+	.on('click', function(e) {
+
+		let $this = $(this),
+			$body = $this.parent('.js-add-content');
+
+		$body
+			// .velocity('stop')
+			.velocity("slideUp", {
+				duration: 300,
+				complete: function() {
+					$this.removeClass('js-add-content--open');
+				}
+			})
 	})
